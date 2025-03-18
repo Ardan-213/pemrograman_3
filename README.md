443431b3c682eda36cfeba9ccac204168afdb97d
ardan-notbad <dutaardan635@gmail.com>
2025-03-18 14:23:14 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+## Backend NodeJS + MongoDB
+
+Paduan Download dan Install Dependensi yang dibbutuhkan dengan cara dibawah
+
+Setelah itu local server akan berjalan di port 3000
+
+http://localhost:3000
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = require("./routes/routes");
+const app = express();
+
+app.use (express.json());
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://0.0.0.0:27017/belajar_mongodb2")
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log("Failed to connect to MongoDB: ", error);
+});
+
+database.once('connected', () => {
+  console.log("Connected to MongoDB");
+});
+
+app.use("/", router);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
